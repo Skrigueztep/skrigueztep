02029ee0a77671aeee4c6570e9a69c3a03b0403d
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2020-04-27 15:10:56 -0500

    feat: Added routerLink to /cv

--- src/app/share/components/components.module.ts
+++ src/app/share/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {RouterModule} from '@angular/router';
 
 const COMPONENTS = [
   NavBarComponent
@@ -9,7 +10,8 @@ const COMPONENTS = [
 @NgModule({
   declarations: [COMPONENTS],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     COMPONENTS
