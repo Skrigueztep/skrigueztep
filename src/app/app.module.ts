e1d329a26cd94b106dc9d4c360a97e98f94e776d
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2020-04-27 13:39:29 -0500

    refactor: app-routing.module.ts to app.routes.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutesModule } from './app.routes.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -11,7 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutesModule,
     BrowserAnimationsModule
   ],
   providers: [],
