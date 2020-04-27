06499422a3ea36e0cd7989a033a638485cb871f8
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2020-04-27 15:01:56 -0500

    feat(landing.module): Material Module added

--- src/app/ui/landing/landing.module.ts
+++ src/app/ui/landing/landing.module.ts
@@ -4,14 +4,15 @@ import { CommonModule } from '@angular/common';
 import { LandingRoutesModule } from './landing.routes.module';
 import { LandingComponent } from './pages/landing/landing.component';
 import {ShareModule} from '../../share/share.module';
-
+import {MaterialModule} from '../../core/modules/material/material.module';
 
 @NgModule({
   declarations: [LandingComponent],
   imports: [
     CommonModule,
     LandingRoutesModule,
-    ShareModule
+    ShareModule,
+    MaterialModule
   ]
 })
 export class LandingModule { }
