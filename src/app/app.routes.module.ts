e85d9f0e7ca56c49d53287d81f93b7c27fd21198
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2020-04-27 15:06:14 -0500

    feat: CV Module added

--- src/app/app.routes.module.ts
+++ src/app/app.routes.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./ui/landing/landing.module').then(m => m.LandingModule) }
+  { path: '', loadChildren: () => import('./ui/landing/landing.module').then(m => m.LandingModule) },
+  { path: 'cv', loadChildren: () => import('./ui/cv/cv.module').then(m => m.CvModule) }
 ];
 
 @NgModule({
